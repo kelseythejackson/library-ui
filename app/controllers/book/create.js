c321cd7544035c27cdd0182ef605345839ad5679
Kelsey Jackson <kelseythej@gmail.com>
2019-07-20 14:14:04 -0500

    finished video-31

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -2,8 +2,12 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
     actions: {
-        selectAuthor(author) {
-         console.log(author)   
+        saveBook(ev) {
+            ev.preventDefault()
+            const book = this.store.createRecord('book', this.model);
+            book.save().then(() => {
+                this.transitionToRoute('book');
+            });
         }, 
         searchAuthor(query) {
            return this.store.query('author', { filter: {query}})
