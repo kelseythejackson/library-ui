ea1730820addebd48b337987dddcf9aadb9fb626
Kelsey Jackson <kelseythej@gmail.com>
2019-07-20 01:34:46 -0500

    finished video-15

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model({ id }) {
+        return {
+            first:  'j.k',
+            last: 'Rowling',
+            id: `authors-${id}`
+        }
+    }
 });
